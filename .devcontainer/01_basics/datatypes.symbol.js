abd0826489ad90490db8377d98aa1455626ce482
hrisabh23 <hrisabhsingh13@gmail.com>
2025-02-28 21:15:04 +0000

    datatypes1

---
+++ .devcontainer/01_basics/datatypes.symbol.js
@@ -0,0 +1,27 @@
+// primitive
+
+
+// 7 types:String,Number,Boolean,null,undefined,symbol,Bigint
+
+const Id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(Id==anotherId);
+
+
+//reference (non primitive)
+
+//Array,objects,functions
+
+const a=[];
+
+let myoj={
+    name:"Hrisabh",
+    age:22
+
+}
+
+const myfunstion=function(){
+    console.log("Hello World");
+}
+
