fae9f8a2c6735ce48b866c5d2e8072bde31fe08b
hrisabh23 <hrisabhsingh13@gmail.com>
2025-02-28 20:50:07 +0000

    conversion

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,18 @@
+let score=null
+
+//console.log(typeof score);
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33"=>33
+//"33abc"=>nan
+//true=>1 false=>0
+
+
+let somenumber=44
+let somestring=String(somenumber)
+console.log(somestring);
+console.log(typeof somestring);
