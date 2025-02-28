472ad999462e0131e280dce86f179de9f48d62b4
hrisabh23 <hrisabhsingh13@gmail.com>
2025-02-28 19:53:22 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=144553
+let accountEmail="hrisabhsingh13@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+accountEmail="hc@hc.com"
+accountPassword=""
+
+console.log(accountId);
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using nodejs, not browser
+
+let age=18
+let name="hrisabh"
+let isloggedin=false
+let state;
+
+//numbers
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone
+//undefined//symbol
+//object
+
+console.log(typeof null);
