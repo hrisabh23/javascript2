6a703c92015f724d683747761e97142b4dcfcb7c
hrisabh23 <hrisabhsingh13@gmail.com>
2025-03-03 19:28:07 +0000

    strings

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,31 @@
+//const name="hrisabh"
+//const repocount=50
+
+//console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gamename=new String('Hrisabh-one-man')
+
+//  console.log(gamename[0]);
+//  console.log(gamename.__proto__);
+//  console.log(gamename.length);
+//  console.log(gamename.toUpperCase());
+//  console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('i'));
+
+const newString=gamename.substring(0,4)
+console.log(newString);
+
+// const anotherstring=gamename.slice(-8,4)
+// console.log(anotherstring);
+
+// const newstring1="  Hrisabh  "
+// console.log(newstring1);
+// console.log(newstring1.trim());
+
+// const url="http://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('.com','.yahoo'));
+
+// console.log(url.includes('sundar'));
+
+console.log(gamename.split('-')); //create a array not having '-' sign
