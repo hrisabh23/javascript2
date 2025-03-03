585aab95ebd3768252f0db0609df75a54f202fe7
hrisabh23 <hrisabhsingh13@gmail.com>
2025-03-03 20:40:46 +0000

    math and num

---
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -0,0 +1,32 @@
+// const score=400
+// console.log(score);
+
+// const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const othernumber=23.8666
+
+// console.log(othernumber.toPrecision(3));
+
+// const hundred=100000
+//console.log(hundred.toLocaleString('en-IN'));
+
+//maths
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
