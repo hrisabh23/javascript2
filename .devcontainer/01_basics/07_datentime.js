9618d33896166a3d8a8b5b7f067702b0d40ddfba
hrisabh23 <hrisabhsingh13@gmail.com>
2025-03-04 18:52:19 +0000

    date and time

---
+++ .devcontainer/01_basics/07_datentime.js
@@ -0,0 +1,20 @@
+//Dates
+
+// let mydate=new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+// let mycreteddate=new Date(2025,0,23)
+// console.log(mycreteddate.toDateString());
+
+let mycreateddate=new Date("01-14-2025")
+// console.log(mycreateddate.toLocaleString());
+
+let mytimestamp=Date.now()
+// onsole.log(mycreateddate.getTime());
+// console.log(mytimestamp);
+cconsole.log(Math.floor(Date.now()/1000));
+
+
